724983e6d2b824c1257cb576dfc41c0eafab45cb
Supritam <supritamsarkar8@gmail.com>
2025-07-19 10:41:18 +0530

    Landing page

--- inspix-landing/vite.config.js
+++ inspix-landing/vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', // 👈 this is important
   plugins: [react(), tailwindcss()],
 })
